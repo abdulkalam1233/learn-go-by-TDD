489c9be18b71bd6ddbdda41a79dd745336ccbe42
Abdul Kalam <abdul@incubyte.co>
2025-04-27 00:58:45 +0530

    feat: add Repeat function to iterate a character multiple times and corresponding tests

---
+++ iteration/iteration_test.go
@@ -0,0 +1,18 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	t.Run("Repeat character 5 times ", func(t *testing.T) {
+		got := Repeat("a")
+		expected := "aaaaa"
+		assertCorrectMessage(t, expected, got)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, expected string, got string) {
+	t.Helper() // This will help to identify the test that failed
+	if expected != got {
+		t.Errorf("expected '%s' but got '%s'", expected, got)
+	}
+}
