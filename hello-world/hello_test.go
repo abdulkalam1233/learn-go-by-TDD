e7cc7503f181db09bfe6c7b76c1eca3c4aa6cddc
Abdul Kalam <abdul@incubyte.co>
2025-04-27 00:27:53 +0530

    refactor: simplify test assertions by introducing assertCorrectMessage helper function

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -6,32 +6,31 @@ func TestHello(t *testing.T) {
 	t.Run("Say hello to people", func(t *testing.T) {
 		got := Hello("Chris", "")
 		want := "Hello, Chris"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, world' when name is empty", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Greet in spanish", func(t *testing.T) {
 		got := Hello("Elodie", "Spanish")
 		want := "Hola, Elodie"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Greet in French", func(t *testing.T) {
 		got := Hello("Elodie", "French")
 		want := "Bonjour, Elodie"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got string, want string) {
+	t.Helper() // This will help to identify the test that failed
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
