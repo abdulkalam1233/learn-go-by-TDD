48298c6e5f0c94f3d48360f098977194984c04b0
Abdul Kalam <abdul@incubyte.co>
2025-04-29 10:45:05 +0530

    feat: update Sum function to accept a slice of integers and enhance tests for variable input sizes

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,14 +4,21 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
-		given := [5]int{1, 2, 3, 4, 5}
+		given := []int{1, 2, 3, 4, 5}
 		got := Sum(given)
 		want := 15
 		assertCorrectMessage(t, got, want, given)
 	})
+
+	t.Run("Collection of any size", func(t *testing.T) {
+		given := []int{1, 2, 3}
+		got := Sum(given)
+		want := 6
+		assertCorrectMessage(t, got, want, given)
+	})
 }
 
-func assertCorrectMessage(t *testing.T, got int, want int, given [5]int) {
+func assertCorrectMessage(t *testing.T, got int, want int, given []int) {
 	if got != want {
 		t.Errorf("got %d, want %d given %v", got, want, given)
 	}
