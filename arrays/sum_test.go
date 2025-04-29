81d7e612ddc1253a5ae75ddd9e39a541674794b1
Abdul Kalam <abdul@incubyte.co>
2025-04-29 10:45:45 +0530

    test: remove redundant test case for Sum function with a collection of integers

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,17 +3,10 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("Collection of 5 numbers", func(t *testing.T) {
+	t.Run("Collection of any size", func(t *testing.T) {
 		given := []int{1, 2, 3, 4, 5}
 		got := Sum(given)
 		want := 15
-		assertCorrectMessage(t, got, want, given)
-	})
-
-	t.Run("Collection of any size", func(t *testing.T) {
-		given := []int{1, 2, 3}
-		got := Sum(given)
-		want := 6
 		assertCorrectMessage(t, got, want, given)
 	})
 }
