48ba3a14e00c27f56b7414b8a18e26e84b459c2b
Abdul Kalam <abdul@incubyte.co>
2025-05-03 21:54:14 +0530

    refactor: extract checkSums function to reduce redundancy in tests for SumAll and SumAllAtTail

--- arrays/sum_all_test.go
+++ arrays/sum_all_test.go
@@ -9,26 +9,35 @@ func TestSumAll(t *testing.T) {
 	t.Run("Addition of all numbers in an array", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{3, 3})
 		want := []int{3, 6}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
 
 func TestSumAllAtTail(t *testing.T) {
+	//checkSums := func(t *testing.T, got, want []int) {
+	//	t.Helper()
+	//	if !reflect.DeepEqual(got, want) {
+	//		t.Errorf("got %v, want %v", got, want)
+	//	}
+	//}
+
 	t.Run("Addition of numbers at tail", func(t *testing.T) {
 		got := SumAllAtTail([]int{1, 2}, []int{3, 3})
 		want := []int{2, 3}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("Addition of empty array", func(t *testing.T) {
 		got := SumAllAtTail([]int{}, []int{3, 3})
 		want := []int{0, 3}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
+
+}
+
+func checkSums(t *testing.T, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
