c291be5c87cbd1fcee7c2e5b7e6c92b245ac00c2
Abdul Kalam <abdul@incubyte.co>
2025-05-03 22:39:05 +0530

    feat: add Triangle type with Area method and update tests to include Triangle

--- types/shapes.go
+++ types/shapes.go
@@ -26,3 +26,12 @@ func (r Rectangle) Area() float64 {
 func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
