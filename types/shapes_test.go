b83a4f8ed08d357c24922d6d296241f4bd881acd
Abdul Kalam <abdul@incubyte.co>
2025-05-03 22:25:49 +0530

    feat: add Circle type with Area method and update Rectangle Area method to use a receiver

--- types/shapes_test.go
+++ types/shapes_test.go
@@ -13,10 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.00, 10.00}
-	got := Area(rectangle)
-	want := 100.00
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+
+	t.Run("Area of rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.00, 10.00}
+		got := rectangle.Area()
+		want := 100.00
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("Area of circle", func(t *testing.T) {
+		circle := Circle{10.00}
+		got := circle.Area()
+		want := 314.00
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
 }
