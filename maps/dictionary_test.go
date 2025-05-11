d1f64a53c19965b8232ec4853bd67674feb38691
Abdul Kalam <abdul@incubyte.co>
2025-05-11 20:05:17 +0530

    feat: add TestAdd function to dictionary tests and improve definition assertion

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -23,19 +23,30 @@ func TestDictionary(t *testing.T) {
 		}
 		assertErrors(t, got, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
 
 	t.Run("Adding a word", func(t *testing.T) {
-		dictionary = Dictionary{}
-		dictionary.Add("test", "Test something")
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "Test something"
+		dictionary.Add(word, definition)
 
-		got, err := dictionary.Search("test")
-		want := "Test something"
-		if err != nil {
-			t.Fatal("should find added word:", err)
-		}
-
-		assertStrings(t, got, want)
+		assertDefinition(t, dictionary, word, definition)
 	})
+
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word string, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, definition)
 }
 
 func assertStrings(t *testing.T, got string, want string) {
